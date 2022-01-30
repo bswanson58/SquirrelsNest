33c4027465c19e02abf95b62fe871d33d97d3014
Bill Swanson <BSwanson58@gmail.com>
2022-01-30 10:29:05 -0600

    Initial bare project structure.

---
+++ SquirrelsNest.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SquirrelsNest.Desktop {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
