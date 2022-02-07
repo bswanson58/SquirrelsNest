2944c156a72fc6f43b8caa4658e0af5d82eef4cb
Bill Swanson <BSwanson58@gmail.com>
2022-02-07 11:35:34 -0600

    Created a class to encapsulate IDs for entities.

---
+++ SquirrelsNest.Common.Tests/Types/EntityIdTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentAssertions;
+using SquirrelsNest.Common.Values;
+using Xunit;
+
+namespace SquirrelsNest.Common.Tests.Types {
+    public class EntityIdTests {
+        [Fact]
+        public void NullId() {
+            var sut = EntityId.For( String.Empty );
+
+            sut.IsSome.Should().BeFalse( "Empty string should be an invalid entity ID." );
+        }
+
+        [Fact]
+        public void WhiteSpaceId() {
+            var sut = EntityId.For( " " );
+
+            sut.IsNone.Should().BeTrue( "White space is not a valid entity ID" );
+        }
+
+        [Fact]
+        public void ValidId() {
+            var sut = EntityId.For( "Valid ID" );
+
+            sut.IsSome.Should().BeTrue( "Any non white space string should be a valid entity ID" );
+        }
+
+        [Fact]
+        public void HasValidValue() {
+            var entityId = "entity-1";
+            var sut = EntityId.For( entityId );
+
+            var _ = sut.Some( entity => entity.Value.Should().Be( entityId, "entity ID should encapsulate value given" ));
+        }
+
+        [Fact]
+        public void EntityCanBeString() {
+            var entityId = "entity-1";
+            var sut = EntityId.For( entityId );
+            var sutEntityId = String.Empty;
+
+            var _ = sut.Some( entity => sutEntityId = entity );
+
+            sutEntityId.Should().BeEquivalentTo( sutEntityId, "EntityId should allow conversion to string." );
+        }
+    }
+}
---
+++ SquirrelsNest.Common/Values/EntityId.cs
@@ -0,0 +1,27 @@
+﻿using LanguageExt;
+
+namespace SquirrelsNest.Common.Values {
+    public class EntityId {
+        public  string  Value { get; }
+
+        public static implicit operator string( EntityId issueId ) => issueId.Value;
+
+        private EntityId( string value ) {
+            if(!IsValid( value )) {
+                throw new ArgumentNullException( value );
+            }
+
+            Value = value;
+        }
+
+        private static bool IsValid( string value ) {
+            return !String.IsNullOrWhiteSpace( value );
+        }
+
+        public static Option<EntityId> For( string value ) => 
+            IsValid( value ) ? Option<EntityId>.Some( new EntityId( value )) : 
+                Option<EntityId>.None;
+
+        internal static EntityId Default => new EntityId( "default" );
+    }
+}
