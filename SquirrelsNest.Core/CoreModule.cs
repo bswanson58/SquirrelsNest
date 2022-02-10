ecd198398acb07943ed46cc900672e77f3d734aa
Bill Swanson <BSwanson58@gmail.com>
2022-02-10 10:53:21 -0600

    Created IOC modules in the Core and LiteDb assemblies that are usable for application startup.

---
+++ SquirrelsNest.Core/CoreModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+using SquirrelsNest.Common.Interfaces;
+using SquirrelsNest.Core.Environment;
+
+namespace SquirrelsNest.Core {
+    public class CoreModule : Module {
+        protected override void Load( ContainerBuilder builder ) {
+            builder.RegisterType<ApplicationConstants>().As<IApplicationConstants>();
+            builder.RegisterType<ApplicationEnvironment>().As<IEnvironment>();
+        }
+    }
+}
---
+++ SquirrelsNest.LiteDb/LiteDbModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using SquirrelsNest.Common.Interfaces;
+using SquirrelsNest.LiteDb.Database;
+using SquirrelsNest.LiteDb.Providers;
+
+namespace SquirrelsNest.LiteDb {
+    public class LiteDbModule : Module {
+        protected override void Load( ContainerBuilder builder ) {
+            builder.RegisterType<DatabaseProvider>().As<IDatabaseProvider>();
+            builder.RegisterType<ProjectProvider>().As<IProjectProvider>();
+        }
+    }
+}
