d0dd91807d0162d2049afc77f4f1b8d551f4227a
Bill Swanson <BSwanson58@gmail.com>
2022-02-10 13:10:09 -0600

    Added project list/selection to the IssueListFilter view/view model.

--- SquirrelsNest.Desktop/ViewModels/IssueListFilterViewModel.cs
+++ SquirrelsNest.Desktop/ViewModels/IssueListFilterViewModel.cs
@@ -1,7 +1,50 @@
-﻿namespace SquirrelsNest.Desktop.ViewModels {
-    internal class IssueListFilterViewModel {
-        public IssueListFilterViewModel() {
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using MoreLinq;
+using SquirrelsNest.Common.Entities;
+using SquirrelsNest.Common.Interfaces;
 
+namespace SquirrelsNest.Desktop.ViewModels {
+    internal class IssueListFilterViewModel : ObservableObject {
+        private readonly IProjectProvider       mProjects;
+        private SnProject ?                     mCurrentProject;
+
+        public  ObservableCollection<SnProject> ProjectList { get; }
+        public  IRelayCommand                   CreateProject { get; }
+
+        public IssueListFilterViewModel( IProjectProvider projects ) {
+            mProjects = projects;
+
+            ProjectList = new ObservableCollection<SnProject>();
+
+            CreateProject = new RelayCommand( OnCreateProject );
+
+            LoadProjects();
         }
+
+        public SnProject ?  CurrentProject {
+            get => mCurrentProject;
+            set => SetProperty( ref mCurrentProject, value );
+        }
+
+        private void LoadProjects() {
+            var currentProject = CurrentProject;
+
+            ProjectList.Clear();
+
+            mProjects.GetProjects()
+                .Do( projectList => projectList.ForEach( project => ProjectList.Add( project )))
+                .IfLeft( error => { });
+
+            if( currentProject != null ) {
+                CurrentProject = ProjectList.FirstOrDefault( p => p.EntityId.Equals( currentProject.EntityId ));
+            }
+
+            CurrentProject ??= ProjectList.FirstOrDefault();
+        }
+
+        private void OnCreateProject() { }
     }
 }
