4dacf005fb3efafbe3cbff259f3d5c3ae8b5877e
Bill Swanson <BSwanson58@gmail.com>
2022-02-13 11:56:45 -0600

    Basic display of issues in the IssueListView.

--- SquirrelsNest.Desktop/ViewModels/IssueListViewModel.cs
+++ SquirrelsNest.Desktop/ViewModels/IssueListViewModel.cs
@@ -1,7 +1,44 @@
-﻿namespace SquirrelsNest.Desktop.ViewModels {
-    internal class IssueListViewModel {
-        public IssueListViewModel() {
+﻿using System;
+using System.Collections.ObjectModel;
+using LanguageExt.Common;
+using MoreLinq;
+using SquirrelsNest.Common.Entities;
+using SquirrelsNest.Common.Interfaces;
+using SquirrelsNest.Desktop.Models;
 
+namespace SquirrelsNest.Desktop.ViewModels {
+    internal class IssueListViewModel : IDisposable {
+        private readonly IIssueProvider     mIssueProvider;
+        private readonly IModelState        mModelState;
+        private IDisposable ?               mModelStateSubscription;
+
+        public  ObservableCollection<SnIssue>   IssueList { get; }
+
+        public IssueListViewModel( IModelState modelState, IIssueProvider issueProvider ) {
+            mModelState = modelState;
+            mIssueProvider = issueProvider;
+
+            IssueList = new ObservableCollection<SnIssue>();
+
+            mModelStateSubscription = mModelState.OnStateChange.Subscribe( OnModelStateChanged );
+        }
+
+        private void OnModelStateChanged( CurrentState state ) {
+            LoadIssueList( state );
+        }
+
+        private void LoadIssueList( CurrentState forState ) {
+            IssueList.Clear();
+
+            forState.Project.ToEither( new Error())
+                .Bind( project => mIssueProvider.GetIssues( project ))
+                .Match( list => list.ForEach( i => IssueList.Add( i )),
+                        error => { });
+        }
+
+        public void Dispose() {
+            mModelStateSubscription?.Dispose();
+            mModelStateSubscription = null;
         }
     }
 }
