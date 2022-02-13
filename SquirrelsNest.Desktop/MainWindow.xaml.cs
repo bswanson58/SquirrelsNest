8aef00e31b1698499342cd50abaab9f9a1785f0a
Bill Swanson <BSwanson58@gmail.com>
2022-02-13 15:36:02 -0600

    Added window placement/restore and a settings file to store the values.

--- SquirrelsNest.Desktop/MainWindow.xaml.cs
+++ SquirrelsNest.Desktop/MainWindow.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace SquirrelsNest.Desktop {
+﻿using System;
+using System.ComponentModel;
+using SquirrelsNest.Desktop.Platform;
+using SquirrelsNest.Desktop.Properties;
+
+namespace SquirrelsNest.Desktop {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
+
+            Closing += OnWindowClosing;
+        }
+
+        private void OnWindowClosing( object ? sender, CancelEventArgs args ) {
+            Settings.Default.ApplicationWindowPlacement = this.GetPlacement();
+            Settings.Default.Save();
+        }
+
+        protected override void OnSourceInitialized( EventArgs e ) {
+            base.OnSourceInitialized( e );
+
+            this.SetPlacement( Settings.Default.ApplicationWindowPlacement );
         }
     }
 }
