467d9de53767e2e77cc8dc83271855ec8155969e
Bill Swanson <BSwanson58@gmail.com>
2022-02-15 13:10:06 -0600

    Implemented the SnRelease/DbRelease entities.

--- SquirrelsNest.Common/Entities/SnRelease.cs
+++ SquirrelsNest.Common/Entities/SnRelease.cs
@@ -1,4 +1,41 @@
-﻿namespace SquirrelsNest.Common.Entities {
-    public class SnRelease {
+﻿using System.Diagnostics;
+using SquirrelsNest.Common.Platform;
+
+namespace SquirrelsNest.Common.Entities {
+    [DebuggerDisplay("Release = {" + nameof( Version ) + "}")]
+    public class SnRelease : EntityBase {
+        public  string      Version { get; }
+        public  string      Description { get; }
+        public  string      RepositoryLabel { get; }
+        public  DateOnly    ReleaseDate { get; }
+
+        // the serializable constructor
+        public SnRelease( string entityId, string dbId, string version, string description, string repositoryLabel, DateOnly releaseDate )
+            : base( entityId, dbId ) {
+            Version = version;
+            Description = description;
+            RepositoryLabel = repositoryLabel;
+            ReleaseDate = releaseDate;
+        }
+
+        public SnRelease( string version )
+            : base( String.Empty ) {
+            Version = version;
+
+            Description = String.Empty;
+            RepositoryLabel = String.Empty;
+
+            ReleaseDate = DateTimeProvider.Instance.CurrentDate;
+        }
+
+        public SnRelease With( string ? version = null, string ? description = null, string ? repositoryLabel = null, DateOnly ? releaseDate = null ) {
+            return new SnRelease( 
+                    EntityId, DbId,
+                    version ?? Version,
+                    description ?? Description,
+                    repositoryLabel ?? RepositoryLabel,
+                    releaseDate ?? ReleaseDate
+                );
+        }
     }
 }
--- SquirrelsNest.LiteDb/Dto/DbRelease.cs
+++ SquirrelsNest.LiteDb/Dto/DbRelease.cs
@@ -1,7 +1,34 @@
 ﻿using LiteDB;
+using SquirrelsNest.Common.Entities;
+using SquirrelsNest.Common.Platform;
 
 namespace SquirrelsNest.LiteDb.Dto {
     internal class DbRelease : DbBase {
-        protected DbRelease() { }
+        public  string      Version { get; set;}
+        public  string      Description { get; set; }
+        public  string      RepositoryLabel { get; set; }
+        public  DateOnly    ReleaseDate { get; set; }
+
+        protected DbRelease() {
+            Version = String.Empty;
+            Description = String.Empty;
+            RepositoryLabel = String.Empty;
+            ReleaseDate = DateTimeProvider.Instance.CurrentDate;
+        }
+
+        public static DbRelease From( SnRelease release ) {
+            return new DbRelease {
+                EntityId = release.EntityId,
+                Id = String.IsNullOrWhiteSpace( release.DbId ) ? ObjectId.NewObjectId() : new ObjectId( release.DbId ),
+                Version = release.Version,
+                Description = release.Description,
+                RepositoryLabel = release.RepositoryLabel,
+                ReleaseDate = release.ReleaseDate
+            };
+        }
+
+        public SnRelease ToEntity() {
+            return new SnRelease( EntityId, Id.ToString(), Version, Description, RepositoryLabel, ReleaseDate );
+        }
     }
 }
