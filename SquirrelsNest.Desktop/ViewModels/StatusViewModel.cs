ec97cd7022981be9e14a27065f7a54fd7f2ee21c
Bill Swanson <BSwanson58@gmail.com>
2022-02-24 13:17:21 -0600

    Added data folder, app name, version and build date to the status view.

--- SquirrelsNest.Desktop/ViewModels/StatusViewModel.cs
+++ SquirrelsNest.Desktop/ViewModels/StatusViewModel.cs
@@ -1,4 +1,46 @@
-﻿namespace SquirrelsNest.Desktop.ViewModels {
+﻿using System;
+using System.Reflection;
+using Microsoft.Toolkit.Mvvm.Input;
+using SquirrelsNest.Common.Interfaces;
+using SquirrelsNest.Desktop.Platform;
+
+namespace SquirrelsNest.Desktop.ViewModels {
+    // ReSharper disable once ClassNeverInstantiated.Global
     internal class StatusViewModel {
+        private readonly IEnvironment   mEnvironment;
+        private readonly ILog           mLog;
+
+        public	string                  VersionString { get; }
+
+        public  IRelayCommand           OpenDataFolder { get; }
+        
+        public StatusViewModel( IEnvironment environment, ILog log ) {
+            mEnvironment = environment;
+            mLog = log;
+
+            var compileDate = ApplicationInformation.GetLinkerTime( Assembly.GetExecutingAssembly());
+            var version = ApplicationInformation.GetVersion( Assembly.GetExecutingAssembly());
+
+            var compileDateString = $"{compileDate.Month:D2}/{compileDate.Day:D2}/{compileDate.Year % 100:D2}";
+            var versionString = version != null ? $"{version.Major}.{version.Minor}" : "unknown";
+
+            VersionString = $"SquirrelsNest v{versionString} - {compileDateString}";
+
+            OpenDataFolder = new RelayCommand( OnOpenDataFolder );
+        }
+
+        private void OnOpenDataFolder() {
+            try {
+                System.Diagnostics.Process.Start( 
+                    new System.Diagnostics.ProcessStartInfo {
+                            FileName = mEnvironment.ApplicationDirectory(),
+                            UseShellExecute = true,
+                            Verb = "open"
+                        });
+            }
+            catch( Exception ex ) {
+                mLog.LogException( "OnLaunchRequest:", ex );
+            }
+        }
     }
 }
