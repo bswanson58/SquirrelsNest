d97416a389ad88c6a432aaefd0e19e39d3b6ddd2
Bill Swanson <BSwanson58@gmail.com>
2022-02-24 11:51:39 -0600

    Created a status bar and added it to the MainWindow.

---
+++ SquirrelsNest.Desktop/Views/StatusView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SquirrelsNest.Desktop.Views {
+    /// <summary>
+    /// Interaction logic for StatusView.xaml
+    /// </summary>
+    public partial class StatusView {
+        public StatusView() {
+            InitializeComponent();
+        }
+    }
+}
