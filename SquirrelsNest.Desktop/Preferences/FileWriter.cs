74649bc688668c22c736b741110c64ef9ffdabc4
Bill Swanson <BSwanson58@gmail.com>
2022-02-24 16:13:57 -0600

    Created a set of classes to load/store user preferences.

---
+++ SquirrelsNest.Desktop/Preferences/FileWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace SquirrelsNest.Desktop.Preferences {
+    public interface IFileWriter<T> {
+        T       Load( string filePath );
+        void    Save( string filePath, T toSave );
+    }
+
+    public class FileWriter<T> : IFileWriter<T> where T : new() {
+        public T Load( string filePath ) => 
+            JsonSerializer.Deserialize<T>( File.ReadAllText( filePath )) ?? new T();
+
+        public void Save( string filePath, T settings ) => 
+            File.WriteAllText( filePath, JsonSerializer.Serialize( settings ));
+    }
+
+}
---
+++ SquirrelsNest.Desktop/Preferences/Preferences.cs
@@ -0,0 +1,18 @@
+﻿namespace SquirrelsNest.Desktop.Preferences {
+    public interface IPreferences<T> where T : new() {
+        T   Current { get; set; }
+    }
+
+    public class Preferences<T> : IPreferences<T> where T : new() {
+        private readonly IPreferencesManager<T>     mPreferences;
+
+        public Preferences( IPreferencesManager<T> preferences ) {
+            mPreferences = preferences;
+        }
+
+        public T Current {
+            get => mPreferences.Load();
+            set => mPreferences.Save( value );
+        }
+    }
+}
