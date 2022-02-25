70c0b61ab860a78d2a321fe38d5eba4c64fef4c2
Bill Swanson <BSwanson58@gmail.com>
2022-02-25 17:55:12 -0600

    Created a current user view and added it to the Issues page.

---
+++ SquirrelsNest.Desktop/ViewModels/CurrentUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using SquirrelsNest.Common.Entities;
+using SquirrelsNest.Desktop.Models;
+
+namespace SquirrelsNest.Desktop.ViewModels {
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class CurrentUserViewModel : ObservableObject, IDisposable {
+        private readonly IDisposable    mUserChangedSubscription;
+        private SnUser                  mCurrentUser;
+
+        public  string                  UserName => mCurrentUser.Name;
+
+        public CurrentUserViewModel( IModelState modelState ) {
+            mCurrentUser = SnUser.Default;
+
+            mUserChangedSubscription = modelState.OnUserChange.Subscribe( OnUserChanged );
+        }
+
+        private void OnUserChanged( CurrentState state ) {
+            mCurrentUser = state.User;
+
+            OnPropertyChanged( nameof( UserName ));
+        }
+
+        public void Dispose() {
+            mUserChangedSubscription.Dispose();
+        }
+    }
+}
