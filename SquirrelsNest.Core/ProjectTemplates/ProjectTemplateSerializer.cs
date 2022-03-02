6533eb08c873953ebd76565f3012b9156c4d6c26
Bill Swanson <BSwanson58@gmail.com>
2022-03-02 17:17:51 -0600

    Code cleanup.

--- SquirrelsNest.Core/ProjectTemplates/ProjectTemplateSerializer.cs
+++ SquirrelsNest.Core/ProjectTemplates/ProjectTemplateSerializer.cs
@@ -1,7 +1,5 @@
-﻿using System.Text.Json;
-using LanguageExt;
+﻿using LanguageExt;
 using LanguageExt.Common;
-using LanguageExt.SomeHelp;
 using SquirrelsNest.Common.Interfaces;
 using SquirrelsNest.Core.Platform;
 
@@ -33,30 +31,11 @@ namespace SquirrelsNest.Core.ProjectTemplates {
         }
 
         public Either<Error, ProjectTemplate> LoadTemplate( string fileName ) {
-//            return Prelude.Try( () => mFileWriter.Load<ProjectTemplate>( TemplateFilePath( fileName ))).ToEither( Error.New );
             return mFileWriter.Load<ProjectTemplate>( TemplateFilePath( fileName ));
-        }
-
-        private Either<Error, Unit> Save<T>( string filePath, T settings ) {
-            var x = Prelude.Try( () => {
-                File.WriteAllText( filePath, JsonSerializer.Serialize( settings, new JsonSerializerOptions { WriteIndented = true }));
-
-                return Unit.Default;
-            });
-
-            return x.ToEither( Error.New );
         }
 
         public Either<Error, Unit> SaveTemplate( ProjectTemplate template, string fileName ) {
             return mFileWriter.Save( TemplateFilePath( fileName ), template );
-/*
-
-            return Prelude.Try( () => {
-                    mFileWriter.Save( TemplateFilePath( fileName ), template );
-
-                    return Unit.Default;
-                })
-                .ToEither( Error.New );
-*/        }
+        }
     }
 }
