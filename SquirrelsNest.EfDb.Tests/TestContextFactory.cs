a80f223d02fae48f8c4d28026b66fc77dfaa7210
Bill Swanson <BSwanson58@gmail.com>
2022-03-05 15:37:13 -0600

    Completed the EF IssueProvider and test suite.

---
+++ SquirrelsNest.EfDb.Tests/TestContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SquirrelsNest.EfDb.Context;
+
+namespace SquirrelsNest.EfDb.Tests {
+    internal class TestContextFactory : IContextFactory {
+        public SquirrelsNestDbContext ProvideContext() {
+            var options = new DbContextOptionsBuilder<SquirrelsNestDbContext>()
+                .UseInMemoryDatabase( "TestDB" );
+
+            return new SquirrelsNestDbContext( options.Options );
+        }
+    }
+}
---
+++ SquirrelsNest.EfDb/Providers/IssueProvider.cs
@@ -0,0 +1,25 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+using SquirrelsNest.Common.Entities;
+using SquirrelsNest.Common.Interfaces;
+using SquirrelsNest.Common.Values;
+using SquirrelsNest.EfDb.Context;
+using SquirrelsNest.EfDb.Dto;
+
+namespace SquirrelsNest.EfDb.Providers {
+    internal class IssueProvider : EntityProvider<SnIssue, DbIssue>, IIssueProvider {
+        public IssueProvider( IContextFactory contextFactory )
+            : base( contextFactory ) { }
+
+        protected override SnIssue ConvertTo( DbIssue issue ) => issue.ToEntity();
+        protected override DbIssue ConvertFrom( SnIssue issue ) => DbIssue.From( issue );
+
+        public Task<Either<Error, SnIssue>> AddIssue( SnIssue issue ) => AddEntity( issue );
+        public Task<Either<Error, Unit>> UpdateIssue( SnIssue issue ) => UpdateEntity( issue );
+        public Task<Either<Error, Unit>> DeleteIssue( SnIssue issue ) => DeleteEntity( issue );
+        public Task<Either<Error, SnIssue>> GetIssue( EntityId issueId ) => GetEntity( issueId );
+        public Task<Either<Error, IEnumerable<SnIssue>>> GetIssues() => GetEntities();
+        public Task<Either<Error, IEnumerable<SnIssue>>> GetIssues( SnProject forProject ) => 
+            GetEntities( c => c.ProjectId.Equals( forProject.EntityId ));
+    }
+}
