b70c6d2c9020f28b099f1cbedcdcbf6cf908bd88
Bill Swanson <BSwanson58@gmail.com>
2022-03-06 13:45:12 -0600

    Added a context factory to the EF database assembly to provide support for command line migrations.

---
+++ SquirrelsNest.EfDb/SquirrelsNestDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SquirrelsNest.EfDb {
+    // For EF Migrations
+    internal class SquirrelsNestDbContextFactory : IDesignTimeDbContextFactory<SquirrelsNestDbContext> {
+        public SquirrelsNestDbContext CreateDbContext( string[] args ) {
+            var optionsBuilder = new DbContextOptionsBuilder<SquirrelsNestDbContext>();
+            optionsBuilder.UseSqlServer( "Server=(localdb)\\MSSQLLocalDB;Database=SquirrelsNestDB;Trusted_Connection=True;MultipleActiveResultSets=true" );
+
+            return new SquirrelsNestDbContext( optionsBuilder.Options );
+        }
+    }
+}
