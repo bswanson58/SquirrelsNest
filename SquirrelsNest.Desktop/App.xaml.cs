9c36ec416ae608d1709f3c1ca464309098c99fc9
Bill Swanson <BSwanson58@gmail.com>
2022-03-07 19:53:38 -0600

    Registered unhandled exception handlers in the app startup.

--- SquirrelsNest.Desktop/App.xaml.cs
+++ SquirrelsNest.Desktop/App.xaml.cs
@@ -1,9 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
 using MvvmSupport;
+using SquirrelsNest.Common.Interfaces;
 using SquirrelsNest.Core;
 using SquirrelsNest.Desktop.Ioc;
 using SquirrelsNest.Desktop.Models;
+using SquirrelsNest.EfDb;
 using SquirrelsNest.LiteDb;
 
 namespace SquirrelsNest.Desktop {
@@ -12,9 +17,14 @@ namespace SquirrelsNest.Desktop {
     /// </summary>
     public partial class App {
         private readonly IDependencyContainer   mContainer;
+        private ILog ?                          mLog;
 
         public App() {
             mContainer = new DependencyContainer();
+
+            DispatcherUnhandledException += AppDispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException +=CurrentDomainUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerUnobservedTaskException;
         }
 
         protected override void OnStartup( StartupEventArgs e ) {
@@ -30,8 +40,35 @@ namespace SquirrelsNest.Desktop {
                 .RegisterViewModelLocator()
                 .BuildDependencies();
 
+            mLog = mContainer.Resolve<ILog>();
+
             mContainer.Resolve<Startup>()
                 .StartApplication(() => mContainer.Stop());
+        }
+
+        private void CurrentDomainUnhandledException( object sender, UnhandledExceptionEventArgs e ) {
+            if( e.ExceptionObject is Exception exception ) {
+                mLog?.LogException( "Application Domain unhandled exception", exception );
+            }
+
+            Shutdown( -1 );
+        }
+
+        private void AppDispatcherUnhandledException( object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e ) {
+            if( Debugger.IsAttached ) {
+                Clipboard.SetText( e.Exception.ToString());
+            }
+
+            mLog?.LogException( "Application Dispatcher unhandled exception", e.Exception );
+
+            e.Handled = true;
+            Shutdown( -1 );
+        }
+
+        private void TaskSchedulerUnobservedTaskException( object ? sender, UnobservedTaskExceptionEventArgs e ) {
+            mLog?.LogException( "Task Scheduler unobserved exception", e.Exception );
+
+            e.SetObserved(); 
         }
     }
 }
