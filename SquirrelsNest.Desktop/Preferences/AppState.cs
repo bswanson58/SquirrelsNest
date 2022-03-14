8b985e660eec08521d95fda446bbb9fbc44ec676
Bill Swanson <BSwanson58@gmail.com>
2022-03-14 16:40:23 -0500

    Removed the preferred project ID from the AppState since it was moved to UserData.

--- SquirrelsNest.Desktop/Preferences/AppState.cs
+++ SquirrelsNest.Desktop/Preferences/AppState.cs
@@ -3,17 +3,14 @@
 namespace SquirrelsNest.Desktop.Preferences {
     internal class AppState {
         public  string        UserId { get; set; }
-        public  string        ProjectId { get; set; }
 
         public AppState() {
             UserId = EntityId.Default;
-            ProjectId = EntityId.Default;
         }
 
         public AppState With( EntityId ? userId = null, EntityId ? projectId = null ) {
             return new AppState {
                 UserId = userId ?? UserId,
-                ProjectId = projectId ?? ProjectId
             };
         }
     }
