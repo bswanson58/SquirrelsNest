496fba56d5799401f6c4a9b0512749c7fce92170
Bill Swanson <BSwanson58@gmail.com>
2022-03-24 15:31:23 -0500

    Added basic layout to the UserInfo component.

--- SquirrelsNest.Acorn/src/components/UserInfo.tsx
+++ SquirrelsNest.Acorn/src/components/UserInfo.tsx
@@ -1,6 +1,21 @@
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import { Stack } from '@mui/material'
+
 function UserInfo() {
   return (
-    <div>UserInfo</div>
+    <Grid
+      container
+      justifyContent='center'
+      sx={{ padding: '15px', bgcolor: 'red' }}>
+      <Stack>
+        <Avatar sx={{ width: 100, height: 100 }}>BS</Avatar>
+        <Box style={{ display: 'flex', justifyContent: 'center' }}>
+          <p>Bill Swanson</p>
+        </Box>
+      </Stack>
+    </Grid>
   )
 }
 
