54b0f910f04908c4eada520540b97226bc511e1e
Bill Swanson <BSwanson58@gmail.com>
2022-03-31 12:00:02 -0500

    Added classes to support authentication.

---
+++ SquirrelsNest.Acorn/src/security/AuthorizedSwitch.tsx
@@ -0,0 +1,26 @@
+import { ReactElement, useEffect, useContext, useState } from 'react'
+import AuthenticationContext from './AuthenticationContext'
+
+interface authorizedProps {
+  authorized: ReactElement
+  notAuthorized?: ReactElement
+  role?: string
+}
+
+export default function Authorized(props: authorizedProps) {
+  const [isAuthorized, setIsAuthorized] = useState( true )
+  const { claims } = useContext( AuthenticationContext )
+
+  useEffect(() => {
+    if (props.role) {
+      const index = claims.findIndex(
+        (claim) => claim.name === 'role' && claim.value === props.role
+      )
+      setIsAuthorized(index > -1)
+    } else {
+      setIsAuthorized(claims.length > 0)
+    }
+  }, [claims, props.role])
+
+  return <>{isAuthorized ? props.authorized : props.notAuthorized}</>
+}
