b60fc296f6ed0fc5cca9d7ae88cb04f8a69622ae
Bill Swanson <BSwanson58@gmail.com>
2022-04-02 15:21:31 -0500

    Cleared the app default theme properties.

--- SquirrelsNest.Acorn/src/theme.ts
+++ SquirrelsNest.Acorn/src/theme.ts
@@ -1,18 +1,9 @@
 import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+
+declare module '@mui/material/styles/createTheme' {
+}
 
 const theme = createTheme({
-    palette:{
-        primary:{
-            main: '#556cd6',
-        },
-        secondary: {
-            main: '#19857b',
-        },
-        error:{
-            main: red.A400,
-        },
-    },
-});
+})
 
-export default theme;
+export default theme
