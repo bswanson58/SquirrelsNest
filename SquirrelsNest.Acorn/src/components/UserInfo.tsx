f23055bba07952eab71d2d78c23fd195270d0662
Bill Swanson <BSwanson58@gmail.com>
2022-04-04 10:38:23 -0500

    Added functionality to the UserInfo component.

--- SquirrelsNest.Acorn/src/components/UserInfo.tsx
+++ SquirrelsNest.Acorn/src/components/UserInfo.tsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react'
 import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import { Stack } from '@mui/material'
+import UserContext from '../security/UserContext'
 
 function UserInfo() {
+  const { user } = useContext(UserContext)
+
   return (
     <Grid
       container
@@ -12,7 +16,7 @@ function UserInfo() {
       <Stack>
         <Avatar sx={{ width: 100, height: 100 }}>BS</Avatar>
         <Box style={{ display: 'flex', justifyContent: 'center' }}>
-          <p>Bill Swanson</p>
+          <p>{user.name}</p>
         </Box>
       </Stack>
     </Grid>
