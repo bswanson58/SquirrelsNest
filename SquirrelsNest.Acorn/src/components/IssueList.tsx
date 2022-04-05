e1577e9481e11ace396a75be2bc77b578dc4ff4c
Bill Swanson <BSwanson58@gmail.com>
2022-04-05 14:44:52 -0500

    Added the description to the issues display.

--- SquirrelsNest.Acorn/src/components/IssueList.tsx
+++ SquirrelsNest.Acorn/src/components/IssueList.tsx
@@ -1,27 +1,47 @@
-import { Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
+import Typography from '@mui/material/Typography'
 import { useIssueContext } from '../data/IssueContext'
+import { ClIssue } from '../data/GraphQlEntities'
+import styled from 'styled-components'
 
 function IssueList() {
   const currentIssues = useIssueContext()
 
-  if(currentIssues.loadingErrors) {
+  const SubTypography = styled(Typography)`
+    opacity: 0.7;
+//    transform: scale(1);
+//    -webkit-transform-origin-x: 0; // align text left after scaling
+  `
+
+  const createPrimary = (issue: ClIssue) => {
+    return <Typography variant='body1'>{issue.title}</Typography>
+  }
+
+  const createSecondary = (issue: ClIssue) => {
+    return <SubTypography variant='body2'>{issue.description}</SubTypography>
+  }
+
+  if (currentIssues.loadingErrors) {
     return <Box>An error occurred...</Box>
   }
 
   return (
     <>
-    <Typography variant='subtitle2'>Issues</Typography>
+      <Typography variant='subtitle2'>Issues</Typography>
 
       <List dense>
         {currentIssues.issueData.issues.map((item) => (
           <ListItem key={item.id as React.Key} disablePadding>
             <ListItemButton>
-              <ListItemText primary={item.title} />
+              <ListItemText
+                disableTypography={true}
+                primary={createPrimary(item)}
+                secondary={createSecondary(item)}
+              />
             </ListItemButton>
           </ListItem>
         ))}
