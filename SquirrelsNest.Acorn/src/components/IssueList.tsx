0c334c6718ee32df79135f04325ada3538a04956
Bill Swanson <BSwanson58@gmail.com>
2022-04-05 16:15:22 -0500

    Changed the IssuesList button to change the list display style to an icon.

--- SquirrelsNest.Acorn/src/components/IssueList.tsx
+++ SquirrelsNest.Acorn/src/components/IssueList.tsx
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
@@ -7,22 +8,82 @@ import Typography from '@mui/material/Typography'
 import { useIssueContext } from '../data/IssueContext'
 import { ClIssue } from '../data/GraphQlEntities'
 import styled from 'styled-components'
+import { Grid } from '@mui/material'
+import { useState } from 'react'
+
+const RelativeBox = styled(Box)`
+  position: relative;
+`
+const TopRightButton = styled(Button)`
+  position: absolute;
+  right: 0%;
+  top: 0%;
+  padding: 0%;
+`
+const SubTypography = styled(Typography)`
+  opacity: 0.7;
+  // transform: scale(1);
+  // -webkit-transform-origin-x: 0; // align text left after scaling
+  `
+enum eDisplayStyle { TITLE_ONLY, TITLE_DESCRIPTION, FULL_DETAILS }
 
 function IssueList() {
+  const [displayStyle, setDisplayStyle] = useState(eDisplayStyle.TITLE_DESCRIPTION)
   const currentIssues = useIssueContext()
 
-  const SubTypography = styled(Typography)`
-    opacity: 0.7;
-//    transform: scale(1);
-//    -webkit-transform-origin-x: 0; // align text left after scaling
-  `
+  const toggleStyle = () => {
+    switch(displayStyle) {
+      case eDisplayStyle.FULL_DETAILS:
+        setDisplayStyle(eDisplayStyle.TITLE_DESCRIPTION)
+        break
+      case eDisplayStyle.TITLE_DESCRIPTION:
+        setDisplayStyle(eDisplayStyle.TITLE_ONLY)
+        break
+      case eDisplayStyle.TITLE_ONLY:
+        setDisplayStyle(eDisplayStyle.FULL_DETAILS)
+        break
+    }
+  }
+
+  const createSubTypography = (text: String) => {
+    return <SubTypography variant='body2'>{text}</SubTypography>
+  }
 
   const createPrimary = (issue: ClIssue) => {
     return <Typography variant='body1'>{issue.title}</Typography>
   }
 
+  const descriptionDetails = (issue: ClIssue) => {
+    return createSubTypography(issue.description)
+  }
+
+  const fullDetails = (issue: ClIssue) => {
+    return(
+      <>
+        <SubTypography variant='body2'>{issue.description}</SubTypography>
+        <Grid container spacing={1} columns={14}>
+          <Grid item xs={1}/>
+          <Grid item xs={3}>{createSubTypography(issue.issueType.name)}</Grid>
+          <Grid item xs={3}>{createSubTypography(issue.workflowState.name)}</Grid>
+          <Grid item xs={3}>{createSubTypography(issue.component.name)}</Grid>
+          <Grid item xs={3}>{createSubTypography(issue.assignedTo.name)}</Grid>
+          <Grid item xs={1}/>
+        </Grid>
+      </>
+    )
+  }
+
   const createSecondary = (issue: ClIssue) => {
-    return <SubTypography variant='body2'>{issue.description}</SubTypography>
+    switch(displayStyle) {
+      case eDisplayStyle.FULL_DETAILS:
+        return fullDetails(issue)
+
+      case eDisplayStyle.TITLE_DESCRIPTION:
+        return descriptionDetails(issue)
+
+      case eDisplayStyle.TITLE_ONLY:
+        return(null)
+    }
   }
 
   if (currentIssues.loadingErrors) {
@@ -30,8 +91,9 @@ function IssueList() {
   }
 
   return (
-    <>
+    <RelativeBox>
       <Typography variant='subtitle2'>Issues</Typography>
+      <TopRightButton onClick={toggleStyle}>ds</TopRightButton>
 
       <List dense>
         {currentIssues.issueData.issues.map((item) => (
@@ -46,7 +108,7 @@ function IssueList() {
           </ListItem>
         ))}
       </List>
-    </>
+    </RelativeBox>
   )
 }
 
