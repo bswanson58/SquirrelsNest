dc10482a1f5c7707f51ef2bc987c74db892a0b10
Bill Swanson <BSwanson58@gmail.com>
2022-04-22 16:05:45 -0500

    Reformatted the client query statements.

--- SquirrelsNest.Acorn/src/data/queryStatements.ts
+++ SquirrelsNest.Acorn/src/data/queryStatements.ts
@@ -1,56 +1,57 @@
 import {gql} from 'graphql-request'
 
 export const AllProjectsQuery = gql`
-    query ProjectsQuery($skip: Int!, $take: Int!, $order:[ClProjectSortInput!], $where:ClProjectFilterInput) {
-        projectList(
-            skip: $skip, 
-            take: $take, 
-            order: $order, 
-            where: $where ) {
-            pageInfo {
-                hasNextPage
-                hasPreviousPage
-            }
-            items {
-                id
-                name
-                description
-                issuePrefix
-            }
-            totalCount
-        }
-    }`
+  query projectsQuery($skip: Int!, $take: Int!, $order:[ClProjectSortInput!], $where:ClProjectFilterInput) {
+    projectList(
+      skip: $skip,
+      take: $take,
+      order: $order,
+      where: $where ) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+      }
+      items {
+        id
+        name
+        description
+        issuePrefix
+      }
+      totalCount
+    }
+  }`
 
 export const IssuesQuery = gql`
-    query IssuesForProjectQuery($skip: Int!, $take: Int!, $projectId: ID!, $order:[ClIssueSortInput!]) {
-        issueList(
-            skip: $skip
-            take: $take
-            projectId: $projectId
-            order: $order ) {
-            pageInfo {
-                hasNextPage
-                hasPreviousPage
-            }
-            items {
-                id
-                issueNumber
-                title
-                description
-                isFinalized
-                component {
-                    name
-                }
-                assignedTo {
-                    name
-                }
-                workflowState {
-                    name
-                }
-                issueType {
-                    name
-                }
-            }
-            totalCount
+  query issuesQuery($skip: Int!, $take: Int!, $projectId: ID!, $order:[ClIssueSortInput!], $where:ClIssueFilterInput) {
+    issueList(
+      skip: $skip
+      take: $take
+      projectId: $projectId
+      order: $order,
+      where: $where ) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+      }
+      items {
+        id
+        issueNumber
+        title
+        description
+        isFinalized
+        component {
+          name
         }
-    }`
+        assignedTo {
+          name
+        }
+        workflowState {
+          name
+        }
+        issueType {
+          name
+        }
+      }
+      totalCount
+    }
+  }`
