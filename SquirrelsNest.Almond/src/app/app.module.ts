fc78f21ece5471939ef26fce58ccbc16c981a3ff
Bill Swanson <bswanson58@gmail.com>
2022-07-21 12:15:13 -0500

    Split the header and sidebar content out into separate components.

--- SquirrelsNest.Almond/src/app/app.module.ts
+++ SquirrelsNest.Almond/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {HttpClientModule} from '@angular/common/http'
 
 import {AppComponent} from './app.component'
 import {AppRoutingModule} from './app.routing.module'
+import {HeaderComponent} from './Navigation/header/header.component'
 import {IssuesPageComponent} from './Issues/issues-page/issues-page.component'
 import {IssueListComponent} from './Issues/issue-list/issue-list.component'
 import {LoginComponent} from './Auth/login/login.component'
@@ -16,18 +17,21 @@ import {MaterialModule} from './material.module'
 import {ProjectsPageComponent} from './Projects/projects-page/projects-page.component'
 import {ProjectListComponent} from './Projects/project-list/project-list.component'
 import {RegisterComponent} from './Auth/register/register.component'
+import {SidenavComponent} from './Navigation/sidenav/sidenav.component'
 import {UsersPageComponent} from './Users/users-page/users-page.component'
 
 
 @NgModule( {
   declarations: [
     AppComponent,
+    HeaderComponent,
     IssuesPageComponent,
     IssueListComponent,
     LoginComponent,
     ProjectsPageComponent,
     ProjectListComponent,
     RegisterComponent,
+    SidenavComponent,
     UsersPageComponent,
   ],
   imports: [
