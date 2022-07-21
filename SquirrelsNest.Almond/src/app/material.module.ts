a1cc0e40ea9067162720639775123fd8410f5a30
Bill Swanson <bswanson58@gmail.com>
2022-07-21 11:39:26 -0500

    Added navigation links in the side navigation bar.

--- SquirrelsNest.Almond/src/app/material.module.ts
+++ SquirrelsNest.Almond/src/app/material.module.ts
@@ -3,12 +3,13 @@ import {MatButtonModule} from '@angular/material/button'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatIconModule} from '@angular/material/icon'
 import {MatInputModule} from '@angular/material/input'
+import {MatListModule} from '@angular/material/list'
 import {MatSidenavModule} from '@angular/material/sidenav'
 import {MatToolbarModule} from '@angular/material/toolbar'
 
 @NgModule( {
-  imports: [MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSidenavModule, MatToolbarModule],
-  exports: [MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSidenavModule, MatToolbarModule]
+  imports: [MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatSidenavModule, MatToolbarModule],
+  exports: [MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatSidenavModule, MatToolbarModule]
 } )
 export class MaterialModule {
 }
