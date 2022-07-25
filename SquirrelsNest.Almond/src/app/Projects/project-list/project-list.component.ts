196ae56a45210fdb19d84eba402f4d17e3fbb32d
Bill Swanson <bswanson58@gmail.com>
2022-07-25 16:26:29 -0500

    Added basic functionality to the project list component.

--- SquirrelsNest.Almond/src/app/Projects/project-list/project-list.component.ts
+++ SquirrelsNest.Almond/src/app/Projects/project-list/project-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {Observable} from 'rxjs'
+import {ClProject} from '../../Data/graphQlTypes'
+import {ProjectService} from '../projects.service'
 
-@Component({
-  selector: 'app-project-list',
+@Component( {
+  selector: 'sn-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.css']
-})
+} )
 export class ProjectListComponent implements OnInit {
+  projectList$: Observable<ClProject[]>
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor( private projectService: ProjectService ) {
+    this.projectList$ = new Observable<ClProject[]>()
   }
 
+  ngOnInit(): void {
+    this.projectList$ = this.projectService.LoadProjects()
+  }
+
+  onProjectSelected( selected: ClProject ) {
+    console.log( selected )
+  }
 }
