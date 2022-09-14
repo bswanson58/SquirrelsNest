97530eb1e6ef928501affa23468a419793085ad0
Bill Swanson <bswanson58@gmail.com>
2022-09-14 09:59:42 -0500

    Added layout and artwork to the home page.

--- SquirrelsNest.Almond/src/app/material.module.ts
+++ SquirrelsNest.Almond/src/app/material.module.ts
@@ -4,6 +4,7 @@ import {MatCardModule} from '@angular/material/card'
 import {MatCheckboxModule} from '@angular/material/checkbox'
 import {MatRippleModule} from '@angular/material/core'
 import {MatDialogModule} from '@angular/material/dialog'
+import {MatDividerModule} from '@angular/material/divider'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatIconModule} from '@angular/material/icon'
 import {MatInputModule} from '@angular/material/input'
@@ -14,12 +15,12 @@ import {MatToolbarModule} from '@angular/material/toolbar'
 
 @NgModule( {
   imports: [
-    MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatFormFieldModule,
+    MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatFormFieldModule,
     MatIconModule, MatInputModule, MatListModule, MatRippleModule, MatSelectModule,
     MatSidenavModule, MatToolbarModule
   ],
   exports: [
-    MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatFormFieldModule,
+    MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatFormFieldModule,
     MatIconModule, MatInputModule, MatListModule, MatRippleModule, MatSelectModule,
     MatSidenavModule, MatToolbarModule
   ]
