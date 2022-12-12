cac3c649455513d7b48a634ca341d9b2e40fd471
Bill Swanson <bswanson58@gmail.com>
2022-12-12 16:41:00 -0600

    Added the Fluxor package and initialized it.

--- SquirrelsNest.Pecan/Client/Program.cs
+++ SquirrelsNest.Pecan/Client/Program.cs
@@ -1,3 +1,4 @@
+using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SquirrelsNest.Pecan.Client;
@@ -6,9 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>( "#app" );
 builder.RootComponents.Add<HeadOutlet>( "head::after" );
 
-builder.Services.AddHttpClient( "SquirrelsNest.Pecan.ServerAPI", client => client.BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) );
-
+builder.Services.AddHttpClient( "SquirrelsNest.Pecan.ServerAPI", 
+    client => client.BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ));
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped( sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient( "SquirrelsNest.Pecan.ServerAPI" ) );
+builder.Services.AddScoped( sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient( "SquirrelsNest.Pecan.ServerAPI" ));
+
+builder.Services.AddFluxor( options => options.ScanAssemblies( typeof( App ).Assembly ));
 
 await builder.Build().RunAsync();
