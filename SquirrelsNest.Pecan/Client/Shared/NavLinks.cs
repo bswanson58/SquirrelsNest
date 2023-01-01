98a874b7c104e3cf454a06da22fc70c40121ef9f
Bill Swanson <bswanson58@gmail.com>
2023-01-01 15:03:46 -0600

    Added a static class to define routes within the client.

---
+++ SquirrelsNest.Pecan/Client/Shared/NavLinks.cs
@@ -0,0 +1,6 @@
+﻿namespace SquirrelsNest.Pecan.Client.Shared {
+    public static class NavLinks {
+        public const string     Projects = "/projects";
+        public const string     Issues = "/issues";
+    }
+}
