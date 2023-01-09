43e816bdbf0f0be7831c9eeee2a935de64a1d451
Bill Swanson <bswanson58@gmail.com>
2023-01-09 12:36:36 -0600

    Added components for a user management page.

--- SquirrelsNest.Pecan/Client/Shared/NavLinks.cs
+++ SquirrelsNest.Pecan/Client/Shared/NavLinks.cs
@@ -1,7 +1,8 @@
 ﻿namespace SquirrelsNest.Pecan.Client.Shared {
     public static class NavLinks {
-        public const string     Login = "/login";
-        public const string     Projects = "/projects";
-        public const string     Issues = "/issues";
+        public const string     Login       = "/login";
+        public const string     Projects    = "/projects";
+        public const string     Issues      = "/issues";
+        public const string     Users       = "/users";
     }
 }
