1ee53e0214397a3e0a7f4e121250e206970971dd
Bill Swanson <bswanson58@gmail.com>
2023-01-18 18:05:14 -0600

    Initialized the authentication state from local storage when the app starts.

--- SquirrelsNest.Pecan/Client/AppStartup.cs
+++ SquirrelsNest.Pecan/Client/AppStartup.cs
@@ -1,8 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using Fluxor;
 using Microsoft.AspNetCore.Components;
+using SquirrelsNest.Pecan.Client.Auth.Store;
 using SquirrelsNest.Pecan.Client.Auth.Support;
+using SquirrelsNest.Pecan.Client.Constants;
 using SquirrelsNest.Pecan.Client.Projects.Store;
 using SquirrelsNest.Pecan.Client.Shared;
 using SquirrelsNest.Pecan.Client.UserData.Store;
@@ -14,23 +18,34 @@ namespace SquirrelsNest.Pecan.Client {
     }
 
     public class AppStartup : IAppStartup {
+        private readonly AuthFacade             mAuthFacade;
         private readonly ProjectFacade          mProjectFacade;
         private readonly IState<ProjectState>   mProjectState;
         private readonly ITokenRefresher        mTokenSupport;
         private readonly NavigationManager      mNavigationManager;
         private readonly UserDataFacade         mUserDataFacade;
+        private readonly ILocalStorageService   mLocalStorage;
 
-        public AppStartup( ProjectFacade projectFacade, IState<ProjectState> projectState, 
+        public AppStartup( AuthFacade authFacade, ProjectFacade projectFacade, IState<ProjectState> projectState, 
                            ITokenRefresher tokenSupport, NavigationManager navigationManager,
-                           UserDataFacade userDataFacade ) {
+                           UserDataFacade userDataFacade, ILocalStorageService localStorageService ) {
+            mAuthFacade = authFacade;
             mProjectFacade = projectFacade;
             mProjectState = projectState;
             mTokenSupport = tokenSupport;
             mNavigationManager = navigationManager;
             mUserDataFacade = userDataFacade;
+            mLocalStorage = localStorageService;
         }
 
         public async Task OnStartup() {
+            var token = await mLocalStorage.GetItemAsStringAsync( LocalStorageNames.AuthToken );
+            var refresh = await mLocalStorage.GetItemAsStringAsync( LocalStorageNames.RefreshToken );
+
+            if(!String.IsNullOrWhiteSpace( token )) {
+                mAuthFacade.SetInitialAuthToken( token, refresh );
+            }
+
             if( await mTokenSupport.TokenRefreshRequired( 1 )) {
                 mNavigationManager.NavigateTo( NavLinks.Login );
             }
