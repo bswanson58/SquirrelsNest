cfff3974a778a866da4bdc93bd4a7a8878765b38
Bill Swanson <bswanson58@gmail.com>
2023-01-22 16:42:51 -0600

    Created a simple About page for the client.

--- SquirrelsNest.Pecan/Client/Shared/NavLinks.cs
+++ SquirrelsNest.Pecan/Client/Shared/NavLinks.cs
@@ -1,6 +1,7 @@
 ﻿namespace SquirrelsNest.Pecan.Client.Shared {
     public static class NavLinks {
         public const string     Home        = "/";
+        public const string     About       = "/about";
         public const string     Login       = "/login";
         public const string     Projects    = "/projects";
         public const string     Issues      = "/issues";
